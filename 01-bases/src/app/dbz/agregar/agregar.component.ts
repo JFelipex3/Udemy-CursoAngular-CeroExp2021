92c0d9bfaf7451e0ecb08e300d4fd7261e0e17cc
Jhonnatan Felipe <jfelipe.machuca@gmail.com>
2021-09-14 16:35:50 -0300

    Sección 5: Expandiendo nuestras bases - clase 68

---
+++ 01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { Personaje } from '../interfaces/dbz.interfaces';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html'
+})
+export class AgregarComponent {
+
+  @Input() nuevo: Personaje = {nombre: '', poder: 0}; // Recibir desde el padre
+
+  constructor( private dbzService: DbzService) {}
+
+  //@Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter(); //Se utiliza para emitir eventos
+
+  agregar() {
+
+    if(this.nuevo.nombre.trim().length === 0){ return; }
+
+    this.dbzService.agregarPersona(this.nuevo);
+
+    this.nuevo = {nombre: '', poder: 0};
+
+  }
+}
